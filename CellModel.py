0b76d1958714a21fedf63f682e3929dd46358c81
Pau <A01701490@itesm.mx>
2021-11-19 23:01:21 -0600

    Add solution

---
+++ CellModel.py
@@ -0,0 +1,95 @@
+# Importamos las clases que se requieren para manejar los agentes (Agent) y su entorno (Model).
+# Cada modelo puede contener múltiples agentes.
+from typing import Tuple
+from mesa import Agent, Model, model 
+
+# Debido a que necesitamos que existe un solo agente por celda, elegimos ''SingleGrid''.
+from mesa.space import Grid, MultiGrid
+
+# Con ''SimultaneousActivation, hacemos que todos los agentes se activen ''al mismo tiempo''.
+from mesa.time import SimultaneousActivation
+
+# Haremos uso de ''DataCollector'' para obtener información de cada paso de la simulación.
+from mesa.datacollection import DataCollector
+
+# Importamos los siguientes paquetes para el mejor manejo de valores numéricos.
+import numpy as np
+import pandas as pd
+
+# Definimos otros paquetes que vamos a usar para medir el tiempo de ejecución de nuestro algoritmo.
+import time
+import datetime
+import random
+
+
+def get_grid(model):
+    width = model.gridAgents.width
+    height = model.gridAgents.height
+    grid = np.zeros( (width,height) )
+    for i in range(width):
+        for j in range(height):
+            grid[i][j] = model.gridCells[i][j]
+    return grid
+
+
+class CleanAgent(Agent):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.wasCleaning = False
+
+    def step(self):
+        if self.wasCleaning or self.model.gridCells[self.pos[0]][self.pos[1]] == 0:
+            choicePositions = [-1, 0, 1]
+            xPosition = random.choice(choicePositions)
+            if xPosition == 0:
+                choicePositions.remove(0)
+            yPosition = random.choice(choicePositions)
+
+            if self.isValid(self.pos[0]+xPosition, self.pos[1]+yPosition):
+                self.pos = (self.pos[0]+xPosition, self.pos[1]+yPosition)
+                self.model.numMovements += 1
+            
+            self.wasCleaning = False
+        
+        else:
+            self.wasCleaning = True
+            self.model.gridCells[self.pos[0]][self.pos[1]] = 0
+            self.model.dirtyCells -= 1
+
+    def isValid(self, x, y):
+        if x >=0 and x< self.model.gridAgents.width and y >= 0 and y < self.model.gridAgents.height:
+            return True
+        return False
+
+
+class CellModel(Model):
+    def __init__(self, width, height, numAgents, dirtyPercentage):
+        self.numAgents = numAgents
+        self.gridAgents = MultiGrid(width, height, False)
+        self.gridCells = np.zeros((width, height), bool)
+        self.schedule = SimultaneousActivation(self)
+        self.dirtyPercentage = dirtyPercentage
+        self.numMovements = 0
+        self.totalCells = width*height
+        self.dirtyCells = np.floor(self.totalCells * dirtyPercentage)
+
+        dirtyCellsLeft = self.dirtyCells
+        for row in range(width):
+            for col in range(height):
+                if dirtyCellsLeft > 0:
+                    self.gridCells[row][col] = 1
+                    dirtyCellsLeft -= 1
+        
+        np.random.shuffle(self.gridCells)
+
+        for agent in range(self.numAgents):
+            a = CleanAgent(agent, self)
+            self.gridAgents.place_agent(a, (0, 0))
+            self.schedule.add(a)
+
+        # Aquí definimos el recolector para obtener el grid completo.
+        self.datacollector = DataCollector(model_reporters={"Grid": get_grid})
+
+    def step(self):
+        self.datacollector.collect(self)
+        self.schedule.step()
---
+++ main.py
@@ -0,0 +1,61 @@
+""" Importamos el modelo del archivo en que lo definimos. """
+from CellModel import CellModel
+
+""" matplotlib lo usaremos crear una animación de cada uno de los pasos
+    del modelo. """
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+plt.rcParams["animation.html"] = "jshtml"
+matplotlib.rcParams['animation.embed_limit'] = 2**128
+
+""" Importamos los siguientes paquetes para el mejor manejo de valores
+    numéricos."""
+import numpy as np
+import pandas as pd
+
+""" Definimos otros paquetes que vamos a usar para medir el tiempo de
+    ejecución de nuestro algoritmo. """
+import time
+import datetime
+
+""" El usuario define el tamaño de la matriz, numero de agentes, 
+    porcentaje de celdas sucias, tiempo maximo de ejecucion """
+M = int(input("No. de filas: "))
+N = int(input("No. de columnas: "))
+numAgents = int(input("No. de agentes: "))
+dirtyCellPercentage = float(input("Porcentaje de celdas sucias inicialmente en decimal: "))
+MAXTIME = int(input("Tiempo máximo de ejecución en segundos: "))
+
+
+""" Registramos el tiempo de inicio y ejecutamos la simulación. """
+start_time = time.time()
+model = CellModel(M, N, numAgents, dirtyCellPercentage)
+
+frames =-1
+while datetime.timedelta(seconds=(time.time() - start_time) < MAXTIME):
+    frames+=1
+    if model.dirtyCells == 0:
+        break
+    model.step()
+
+""" Imprimimos el tiempo que le tomó correr al modelo, y sus atributos al final """
+print('Tiempo de ejecución:', str(datetime.timedelta(seconds=(time.time() - start_time))))
+print('Porcentajede celdas limpias:', (model.totalCells-model.dirtyCells)*100/model.totalCells, "%")
+print('Movimientos realizados por los agentes:', model.numMovements)
+
+""" Obtenemos la información que almacenó el colector, este nos entregará un
+    DataFrame de pandas que contiene toda la información. """
+all_grid = model.datacollector.get_model_vars_dataframe()
+
+# Graficamos la información usando `matplotlib`
+fig, axs = plt.subplots(figsize=(7,7))
+axs.set_xticks([])
+axs.set_yticks([])
+patch = plt.imshow(all_grid.iloc[0][0], cmap=plt.cm.binary)
+
+def animate(i):
+    patch.set_data(all_grid.iloc[i][0])
+
+anim = animation.FuncAnimation(fig, animate, frames=frames)
+plt.show()
